33035f133db6ba92a289402f13fac7ce5ff8b783
unknown <danh1nguyen23@live.com>
2025-09-03 11:05:27 -0700

    military authentication project initalization

---
+++ MiitaryAuthentication/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiitaryAuthentication.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        
+        }
+    }
+}
